31f0159f9bddc20fdb2fca744b5bbeca921f1cf7
Sergey Petrov <petrovsi199912@gmail.com>
2025-01-14 21:27:05 +0300

    second comment

--- precode_test.go
+++ precode_test.go
@@ -19,16 +19,38 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	handler.ServeHTTP(responseRecorder, req)
 
 	// здесь нужно добавить необходимые проверки
-	require.Equal(t, responseRecorder.Code, http.StatusOK)
-	assert.NotEmpty(t, responseRecorder.Body.String())
-
-	city := req.URL.Query().Get("city")
-
-	assert.True(t, true, city)
-
 	body := responseRecorder.Body.String()
 	list := strings.Split(body, ",")
 
 	assert.Len(t, list, totalCount)
+
+}
+
+func TestMainHandlerWhenCityIsNotSupported(t *testing.T) {
+
+	req := httptest.NewRequest("GET", "/cafe?count=4&city=moscow", nil) // здесь нужно создать запрос к сервису
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	// здесь нужно добавить необходимые проверки
+
+	city := req.URL.Query().Get("city")
+	assert.True(t, true, city)
+
+}
+
+func TestMainHandlerWhenRequestCorrectly(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=4&city=moscow", nil) // здесь нужно создать запрос к сервису
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	// здесь нужно добавить необходимые проверки
+
+	require.Equal(t, responseRecorder.Code, http.StatusOK)
+	assert.NotEmpty(t, responseRecorder.Body.String())
 
 }

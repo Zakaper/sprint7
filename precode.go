e1886b2e5226ff38ad32594300357ad9a994de55
Popova Kseniya <kp6331807@gmail.com>
2025-01-22 23:25:19 +0300

    two-commit

--- precode.go
+++ precode.go
@@ -43,6 +43,7 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(answer))
 }
+
 func main() {
 	http.HandleFunc(`/cafe`, mainHandle)
 	err := http.ListenAndServe(":8080", nil)

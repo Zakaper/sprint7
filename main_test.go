3088d2803ca43594ecd08fc472bf32943da3b786
Zakaper <zakabuninpeter@gmail.com>
2025-03-11 23:34:51 +0300

    third commit

--- main_test.go
+++ main_test.go
@@ -5,35 +5,40 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestMainHandleWhenOK(t *testing.T) {
 	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
 
-	responseRecoder := httptest.NewRecorder()
+	responseRecorder := httptest.NewRecorder()
 	handle := http.HandlerFunc(mainHandle)
-	handle.ServeHTTP(responseRecoder, req)
+	handle.ServeHTTP(responseRecorder, req)
 
-	if status := responseRecoder.Code; status != http.StatusOK {
-		t.Errorf("expected status code %d, got %d", http.StatusOK, status)
-	}
+	require.NotEmpty(t, responseRecorder.Code)
+
+	require.Equal(t, responseRecorder.Code, http.StatusOK)
 }
 
 func TestMainHandleWhenMissingCount(t *testing.T) {
 	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
 
-	responseRecoder := httptest.NewRecorder()
+	responseRecorder := httptest.NewRecorder()
 	handle := http.HandlerFunc(mainHandle)
-	handle.ServeHTTP(responseRecoder, req)
+	handle.ServeHTTP(responseRecorder, req)
 
-	if status := responseRecoder.Code; status != http.StatusBadRequest {
-		t.Errorf("expected status code %d, got %d", http.StatusBadRequest, status)
-	}
+	require.NotEqual(t, responseRecorder.Code, http.StatusBadRequest)
+
+	//if status := responseRecoder.Code; status != http.StatusBadRequest {
+	//t.Errorf("expected status code %d, got %d", http.StatusBadRequest, status)
+	//}
 
 	expected := `count missing`
-	if responseRecoder.Body.String() != expected {
-		t.Errorf("expected body: %s, got %s", expected, responseRecoder.Body.String())
-	}
+	require.NotEqual(t, responseRecorder.Body.String(), expected)
+	//if responseRecoder.Body.String() != expected {
+	//t.Errorf("expected body: %s, got %s", expected, responseRecoder.Body.String())
+	//}
 }
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
@@ -44,14 +49,18 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	if status := responseRecorder.Code; status != http.StatusOK {
-		t.Fatalf("expected status code: %d, got %d", http.StatusOK, status)
-	}
+	require.Equal(t, responseRecorder.Code, http.StatusOK)
+
+	//if status := responseRecorder.Code; status != http.StatusOK {
+	//t.Fatalf("expected status code: %d, got %d", http.StatusOK, status)
+	//}
 
 	body := responseRecorder.Body.String()
 	list := strings.Split(body, ",")
 
-	if len(list) != totalCount {
-		t.Errorf("expected cafe count: %d, got %d", totalCount, len(list))
-	}
+	require.NotEqual(t, len(list), totalCount)
+
+	//if len(list) != totalCount {
+	//t.Errorf("expected cafe count: %d, got %d", totalCount, len(list))
+	//}
 }
